0cb8dbcec1d76d3b463edbd6ec6cbd6d895ff8be
Shaurav Parajuli <157129406+Shauravparajuli123@users.noreply.github.com>
2024-12-12 22:43:14 +0545

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slide: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' }, // Moves out from right to left
+        },
+      },
+      animation: {
+        slide: 'slide 8s linear infinite',
+        delay: 'slide 8s linear 4s infinite' // Delay animation start by 4s
+      },
+    },
   },
   plugins: [],
 }
-
